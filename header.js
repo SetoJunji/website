4f80260bd602a67139a61e37ec21ff472a0bfb71
吉田 <debtgx103@gmail.com>
2020-07-22 22:04:07 +0900

    ひとまずコミット

---
+++ header.js
@@ -0,0 +1,4 @@
+function header(){
+	document.write('<style>li{display:inline;padding:10px;background-color:#505050;}</style>');
+	document.write('<h1>共通ヘッダーだぜ!!</h1><ul><li><a href="index.html">ホーム</a></li><li><a href="item.html">アイテム原価</a></li><li><a href="return.html">リターン御礼</a></li></ul>');
+}
