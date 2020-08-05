00ef4d24d86e2d3371dbf8d6ca194d7102a1b237
吉田 <debtgx103@gmail.com>
2020-08-05 22:14:24 +0900

    ひとまずコミット

--- header.js
+++ header.js
@@ -1,83 +1,47 @@
 function header(){
-//<span style="color:green;font-size:260%;">元宝バナナ</span>
-//<span style="float:left;font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
-const css  = String.raw`<style>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {
-  font-family: "Lato", sans-serif;
-}
-
-.sidenav {
-  height: 100%;
-  width: 0;
-  position: fixed;
-  z-index: 1;
-  top: 20%;
-  left: 200px;
-  background-color: #111;
-  overflow-x: hidden;
-  transition: 0.5s;
-  padding-top: 60px;
-}
-
-.sidenav a {
-  padding: 8px 8px 8px 32px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-  transition: 0.1s;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.sidenav .closebtn {
-  position: absolute;
-  top: 0;
-  right: 25px;
-  font-size: 36px;
-  margin-left: 50px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-header {
-  background: skyblue;
-  height: 180px;
-}
-</style>`;
+const css  = String.raw`
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">`;
 
 const html = String.raw`
+<style>
+        /* Modify the background color */
+
+        .navbar-custom {
+            background-color: lightgreen;
+        }
+        /* Modify brand and text color */
+
+        .navbar-custom .navbar-brand,
+        .navbar-custom .navbar-text {
+            color: green;
+        }
+  </style>
 <header>
-<div style="background-color:red;margin-bottom:10px;text-align: center;">
-<span style="float:left;font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
-<span style="color: green;font-size: 260%;">元宝バナナ</span>
-</div>
+<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+  <a class="navbar-brand" href="#">元宝バナナ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">ホーム</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="return.html">リターン御礼</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="item.html">アイテム</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 </header>
-
-<script>
-function openNav() {
-  document.getElementById("mySidenav").style.width = "50%";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-</script>
-
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="index.html">ホーム</a>
-  <a href="return.html">リターン御礼</a>
-  <a href="item.html">アイテム交換</a>
-  <a href="char.html">副将</a>
-  <a href="見た目は整えた.html">新しいページ</a>
-  <a href="bootstrapでメニューバー.html">新しいページ</a>bootstrap頼み...</a>
-</div>`;
+`;
 	document.write(css+html);
 }
